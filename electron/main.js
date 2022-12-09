efb85602967684c4a999eb1a0825d57f9e9010fb
Pedro Victor <pedro20002victor@gmail.com>
2022-12-08 22:28:51 -0300

    build a services

--- electron/main.js
+++ electron/main.js
@@ -8,6 +8,8 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 700,
     height: 600,
+    resizable: false,
+    frame: true,
     webPreferences: path.join(__dirname, "preload.js"),
   });
   win.loadFile("index.html");
@@ -19,31 +21,29 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
-  // const icon = nativeImage.createFromPath("assets/power-bi.svg");
-  tray = new Tray(icon);
+ 
   // Icons in navbar w11
-  const contextMenu = Menu.buildFromTemplate([
-    { label: "Item1", type: "radio", checked: true },
-  ]);
+  // const contextMenu = Menu.buildFromTemplate([
+  //   { label: "Item1", type: "radio", checked: true },
+  // ]);
 
-  tray.setContextMenu(contextMenu);
-  tray.setToolTip("This is my app");
-  tray.setTitle("This is my title");
+  // tray.setContextMenu(contextMenu);
+  // tray.setToolTip("This is my app");
+  // tray.setTitle("This is my title");
 
-  app.do;
 });
 
 // Custom app from taskbar
-app.setUserTasks([
-  {
-    program: process.execPath,
-    arguments: "--new-window",
-    iconPath: process.execPath,
-    iconIndex: 0,
-    title: "New Window teste 1",
-    description: "TEST create a new window",
-  },
-]);
+// app.setUserTasks([
+//   {
+//     program: process.execPath,
+//     arguments: "--new-window",
+//     iconPath: process.execPath,
+//     iconIndex: 0,
+//     title: "New Window teste 1",
+//     description: "TEST create a new window",
+//   },
+// ]);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
---
+++ src/web/services/scraper.js
@@ -0,0 +1,4 @@
+const pup = require('puppeteer');
+// Target: 'https://www.fctables.com/teams/real-madrid-192583/'
+
+const data
