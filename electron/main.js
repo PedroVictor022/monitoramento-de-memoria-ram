f1caffac3c17a80ef028b7e82043b8fdadc0430b
Pedro Victor <pedro20002victor@gmail.com>
2022-12-09 08:18:11 -0300

    start puppeteer

--- electron/main.js
+++ electron/main.js
@@ -1,9 +1,9 @@
-const { app, BrowserWindow, Tray, Menu, nativeImage } = require("electron");
-const pup = require('puppeteer');
+const { app, BrowserWindow, Tray, Menu } = require("electron");
 
+const pup = require('puppeteer');
 const path = require("path");
 
-let tray;
+let tray = null;
 
 // Load this index.html
 const createWindow = () => {
@@ -27,60 +27,45 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
- 
-  // Icons in navbar w11
-  // const contextMenu = Menu.buildFromTemplate([
-  //   { label: "Item1", type: "radio", checked: true },
-  // ]);
+  tray = new Tray('/assets/computer.png')
+  const contextMenu = Menu.buildFromTemplate([
+     { label: 'Item1', type: 'radio' },
+  ])
 
-  // tray.setContextMenu(contextMenu);
-  // tray.setToolTip("This is my app");
-  // tray.setTitle("This is my title");
+  tray.setToolTip('This is my application');
+  tray.setContextMenu(contextMenu);
 
 });
 
-// Custom app from taskbar
-// app.setUserTasks([
-//   {
-//     program: process.execPath,
-//     arguments: "--new-window",
-//     iconPath: process.execPath,
-//     iconIndex: 0,
-//     title: "New Window teste 1",
-//     description: "TEST create a new window",
-//   },
-// ]);
+
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
-// Target: 'https://www.fctables.com/teams/real-madrid-192583/'
-let url = 'https://www.fctables.com/teams/real-madrid-192583/';
-const saveData = [];
-const log = (text) => console.log(text);
+(async() => {
+  const statsData = [];
+  const browser = await pup.launch({
+    headless: true,
+    defaultViewport: false,
+  })
 
-(
-  async() => {
-    // init browser
-    const browser = await pup.launch({
-      headless: true,
-    });
-    const page = await browser.newPage();
+  const page = await browser.newPage();
+  console.log('Browser start');
 
-    log("Browser starts");
-    await page.goto(url);
+  await page.goto("https://www.fctables.com/teams/atletico-mg-180615/");
 
-    await Promise.all([
-      page.waitForSelector()
-    ])
+  const teamInfo = await page.$$("#team-info");
 
-    const title = await page.$(".box-width > h1")
-    console.log(title);
-
-    setInterval(() => {
-      browser.close();
-    }, 3000)
-
+  for(const info of teamInfo) {
+    try {
+      const allInfos = await page.evaluate((el) => el.innerText, info);
+      statsData.push(allInfos);
+    } catch(err) {
+      return `Error - ${err}`;
+    }
   }
-)();
+
+  console.log(statsData);
+
+})();
